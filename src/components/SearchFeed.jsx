90edcd568c4ced411f0eb50d7c6b0f8cccf8cc7e
Adeeb Rahman <adman00794@gmail.com>
2022-10-13 23:13:57 -0500

    fix searchTerm typo

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -24,7 +24,7 @@ const SearchFeed = () => {
         ml={{ sm: "100px" }}
       >
         Search Results for
-        <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+        <span style={{ color: "#FC1503" }}> "{searchTerm}"</span> videos
       </Typography>
       <Box display="flex">
         <Box sm={{ mr: { sm: "100px" } }} />
